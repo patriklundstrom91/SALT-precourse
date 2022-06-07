a04f06126b699ea7d91174080e67995b83639311
Marcus Hammarberg <marcusoft.net@gmail.com>
2022-06-07 13:33:03 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace snallWeeks_lab_firstDotNetApp
+{
+  class Program
+  {
+    public static string Greet(string name)
+    {
+      return "Welcome to SALT!";
+    }
+    static void Main(string[] args)
+    {
+      Console.Write("What is your name? ");
+      var name = Console.ReadLine();
+
+      var greeting = Greet(name);
+
+      Console.WriteLine(greeting);
+    }
+  }
+}
