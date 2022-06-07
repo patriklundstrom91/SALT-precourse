8e988ecc0b141fab5534d5e1b721d858af5d5fdf
Marcus Hammarberg <marcusoft.net@gmail.com>
2022-06-07 15:47:41 +0200

    Minor fixes

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace snallWeeks_lab_firstDotNetApp
 {
   class Program
   {
-    private static string Greet(string name)
+    private static string greet(string name)
     {
       return "Welcome to SALT, ";
     }
@@ -13,7 +13,7 @@ namespace snallWeeks_lab_firstDotNetApp
       Console.Write("What is your name? ");
       var name = Console.ReadLine();
 
-      var greeting = Greet(name);
+      var greeting = greet(name);
 
       Console.WriteLine(greeting);
     }
