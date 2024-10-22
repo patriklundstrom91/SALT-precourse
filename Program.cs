a101c102bd94b4d646ccebecdf13eb75bddf7ecb
Mathias Viklund <mathias.viklund@gmail.com>
2024-10-22 10:38:12 +0200

    Update Program.cs for tutorial

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-﻿static string Greet(string namePassedIn)
-{
-  return "Welcome to SALT, ";
-}
-    
 Console.Write("What is your name? ");
 var name = Console.ReadLine()!;
 
 var greeting = Greet(name);
 
 Console.WriteLine(greeting);
+
+﻿static string Greet(string namePassedIn)
+{
+  return "Welcome to SALT, ";
+}
